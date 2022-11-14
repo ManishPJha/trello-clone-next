57b801d7e1a5b694425a22e94cad223ccae440e5
Manish <mjha205@rku,ac,in>
2022-11-15 01:43:49 +0530

    updated live url

--- utils/checkEnvironment.ts
+++ utils/checkEnvironment.ts
@@ -2,6 +2,6 @@ export const checkEnvironment = () => {
   let BASE_URI =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : "https://trello-clone-next-q5mktox9w-manishpjha.vercel.app";
+      : "https://trello-clone-next-gamma.vercel.app";
   return BASE_URI;
 };
