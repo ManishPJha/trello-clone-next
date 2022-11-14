2364d339db9a56f84dcae03d3326e1234c7fefde
Manish <mjha205@rku,ac,in>
2022-11-15 01:57:12 +0530

    url changes

--- utils/checkEnvironment.ts
+++ utils/checkEnvironment.ts
@@ -2,6 +2,6 @@ export const checkEnvironment = () => {
   let BASE_URI =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : "https://trello-clone-next-gamma.vercel.app";
+      : process.env.VERCEL_URL?.toString();
   return BASE_URI;
 };
