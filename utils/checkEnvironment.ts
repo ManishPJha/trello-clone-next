a47d31cacb3afc5b47e525102fcd6a87eab45246
Manish <mjha205@rku,ac,in>
2022-11-15 02:01:28 +0530

    changes

--- utils/checkEnvironment.ts
+++ utils/checkEnvironment.ts
@@ -2,6 +2,6 @@ export const checkEnvironment = () => {
   let BASE_URI =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : process.env.VERCEL_URL?.toString();
+      : process.env.VERCEL_URL?.toString()!;
   return BASE_URI;
 };
